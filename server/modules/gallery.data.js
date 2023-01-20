a0785d30d3964658171ae01747bf09e28c6296a2
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-01-20 00:32:20 -0600

    modified doggie_school_pic.jpg name, created folder and file for GalleryItem and GalleryList

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,13 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    {id: 2, path: 'images/OrangeSky.jpeg', description: 'Photo of an naturally orange tinted sky, Minnesota', likes: 0},
+    {id: 3, path: 'images/SedonaZo.jpeg', description: 'Photo of a young girl in the mountains of Sedona, AZ in black and white', likes: 0},
+    {id: 4, path: 'images/record_player.jpeg', description: 'Photo of a red record player with a mint green record playing', likes: 0},
+    {id: 5, path: 'images/chalk_drawing.JPG', description: 'Photo of a little girl laying next to a chalk drawing of a flower on asphalt', likes: 0},
+    {id: 6, path: 'images/blue_concert.jpeg', description: 'Photo of a person making a heart shape of their hands at a concert', likes: 0},
+    {id: 7, path: 'images/selfie.jpeg', description: 'Selfie photo of a woman with the sun behind her', likes: 0},
+    {id: 8, path: 'images/doggie_school_pic.jpg', description: 'A school pic styled image of a dog', likes: 0},
+    {id: 9, path: 'images/green_plant.JPG', description: 'Photo of a green plant similar to an artichoke', likes: 0}
 ];
 
 module.exports = galleryItems;
