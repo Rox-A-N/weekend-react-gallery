bcac3b40c6d4b4f3dde98f05e73d116182b8be34
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-01-20 01:24:31 -0600

    used 'axios' to GET data from /gallery and store it in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  let [galleryArray, setGalleryArray] = useState([]);
+
+  useEffect(() => {
+    fetchGallery();
+  }, []);
+
+  const fetchGallery = () => {
+    axios.get('/gallery') // get galleryItems from gallery.data.js
+    .then((response) => {
+      console.log('response from GET galleryItems', response.data);
+      setGalleryArray(response.data);
+    })
+    .catch((error) => {
+      console.log('error getting items', error);
+    })
+  }
+    
+  
+  
     return (
       <div className="App">
         <header className="App-header">
