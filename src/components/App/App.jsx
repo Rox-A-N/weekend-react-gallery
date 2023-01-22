6c304c33af809953795b15ed194f99a58af02729
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-01-21 20:34:29 -0600

    formattin App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,37 +1,46 @@
-import React, { useEffect, useState} from 'react';
-import './App.css';
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  let [galleryArray, setGalleryArray] = useState([]);
+  let [galleryItem, setGalleryItem] = useState([]);
+  const galleryItemProp = useState([]);
 
   useEffect(() => {
     fetchGallery();
   }, []);
 
   const fetchGallery = () => {
-    axios.get('/gallery') // get galleryItems from gallery.data.js
-    .then((response) => {
-      console.log('response from GET galleryItems', response.data);
-      setGalleryArray(response.data);
+    axios({
+      method: "GET",
+      url: "/galleryItems", // get galleryItems from gallery.router.js (I think? or is it galleryItems)
     })
-    .catch((error) => {
-      console.log('error getting items', error);
-    })
-  }
-    
-  
-  
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+      .then((response) => {
+        console.log("response from GET galleryItems", response.data);
+        setGalleryItem(response.data);
+      })
+      .catch((error) => {
+        console.log("error getting items", error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <p>{JSON.stringify(gallery)}</p>
+      <div>
+        {galleryItemProp.map((gallery) => (
+          <li key={gallery.id}>
+            {gallery.path},{gallery.description},{gallery.likes}
+          </li>
+        ))}
       </div>
-    );
+      <img src="images/goat_small.jpg" />
+    </div>
+  );
 }
 
 export default App;
